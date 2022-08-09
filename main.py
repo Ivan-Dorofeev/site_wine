43f333ebc359c40de8fd2c6a61b42c2bbcb91690
Ivan Dorofeev <axxel123@rambler.ru>
2022-08-09 20:10:03 +0300

    Добавил кол-во лет компании

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import datetime
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+template = env.get_template('index.html')
+
+days = 365 * 1920
+years = (datetime.datetime.now() - datetime.timedelta(days=days)).year
+rendered_page = template.render(years=years)
+
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
 
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
