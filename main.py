715848c527f34a7c7339b0717de86dd000ae7d81
Ivan Dorofeev <axxel123@rambler.ru>
2022-08-11 17:22:15 +0300

    Разбил код на функции

--- main.py
+++ main.py
@@ -6,15 +6,10 @@ from pprint import pprint
 import pandas as pd
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
 
-template = env.get_template('template.html')
-
-
-def correct_years_text(years):
+def get_correct_years_text():
+    days = 365 * 1920
+    years = (datetime.datetime.now() - datetime.timedelta(days=days)).year
     names = {
         1: 'год',
         2: 'года',
@@ -27,29 +22,43 @@ def correct_years_text(years):
     return f'{years} {names[int(last_two_numbers)]}'
 
 
-days = 365 * 1920
-years = (datetime.datetime.now() - datetime.timedelta(days=days)).year
-correct_years_text = correct_years_text(years)
+def get_wines_from_file():
+    excel_df = pd.read_excel('wine.xlsx', sheet_name='Лист1', na_values='nan', keep_default_na=False)
+    getted_wines = excel_df.to_dict()
+    count_new_wines = len(getted_wines['Категория'].keys())
 
-excel_df = pd.read_excel('wine.xlsx', sheet_name='Лист1', na_values='nan', keep_default_na=False)
-getted_wines = excel_df.to_dict()
-count_new_wines = len(getted_wines['Категория'].keys())
+    wines = collections.defaultdict(list)
+    for index in range(count_new_wines):
+        category = getted_wines['Категория'][index]
+        wines[category].append({
+            'Картинка': getted_wines['Картинка'][index],
+            'Категория': getted_wines['Категория'][index],
+            'Название': getted_wines['Название'][index],
+            'Сорт': getted_wines['Сорт'][index],
+            'Цена': getted_wines['Цена'][index],
+            'Акция': getted_wines['Акция'][index],
+        })
+    return wines
 
-wines = collections.defaultdict(list)
-for index in range(count_new_wines):
-    category = getted_wines['Категория'][index]
-    wines[category].append({
-        'Картинка': getted_wines['Картинка'][index],
-        'Категория': getted_wines['Категория'][index],
-        'Название': getted_wines['Название'][index],
-        'Сорт': getted_wines['Сорт'][index],
-        'Цена': getted_wines['Цена'][index],
-        'Акция': getted_wines['Акция'][index],
-    })
-pprint(wines)
-rendered_page = template.render(years=correct_years_text, wines=wines)
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+def main():
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+
+    template = env.get_template('template.html')
+
+    correct_years_text = get_correct_years_text()
+    wines = get_wines_from_file()
+
+    rendered_page = template.render(years=correct_years_text, wines=wines)
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()  # place main logic here
+
+
+if __name__ == '__main__':
+    main()
